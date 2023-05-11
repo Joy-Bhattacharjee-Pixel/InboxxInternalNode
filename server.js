d2581dde2b61fb92561d7b5870adcd8fb16e0b29
Joy Bhattacherjee <joy.bhattacharjee@pixelconsultancy.in>
2023-05-11 15:43:15 +0530

    PORT updated

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ require("./app/routes/file.routes")(app); // File route
 
 
 // Defining port
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8045;
 // Listenning to port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
