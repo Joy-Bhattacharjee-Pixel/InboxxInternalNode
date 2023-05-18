a0ba748599c3f3a501d8809723b6b3a295d0990c
Joy Bhattacherjee <joy.bhattacharjee@pixelconsultancy.in>
2023-05-18 13:39:50 +0530

    port updated

--- app/configs/config.js
+++ app/configs/config.js
@@ -1,27 +1,27 @@
 // This file consists all the host, user and db details
-module.exports = {
-  HOST: "localhost",
-  USER: "root",
-  PASSWORD: "",
-  DB: "inboxx",
-  dialect: "mysql",
-  dropboxToken: "sl.BcWxzbcRC_i-cV9aJwD2PG3KF1rfj8RfuqAd1KDrdE-ofilB3aPTEVj79mY4flMSt8CNTm7PYEZ1QBRdaT-1uEZDDJotZKzAu-l5uq7_kyuaUD_XNpOp0cZHig0lOpigh3eOWqDbmmsm",
-  stripeTestKey: "sk_test_Ma7vvXxqIKxpUoHmFNFv2oYt00u1nVjNkF"
-};
-
 // module.exports = {
-//   HOST: "db-mysql-blr1-34758-do-user-14081312-0.b.db.ondigitalocean.com",
-//   USER: "doadmin",
-//   PASSWORD: "AVNS_mxiNlY-OmC3g7jdnbTR",
-//   DB: "defaultdb",
-//   PORT: 25060,
+//   HOST: "localhost",
+//   USER: "root",
+//   PASSWORD: "",
+//   DB: "inboxx",
 //   dialect: "mysql",
-//   dialectOptions: {
-//     ssl: {
-//       require: true,
-//       rejectUnauthorized: false
-//     }
-//   },
+//   dropboxToken: "sl.BcWxzbcRC_i-cV9aJwD2PG3KF1rfj8RfuqAd1KDrdE-ofilB3aPTEVj79mY4flMSt8CNTm7PYEZ1QBRdaT-1uEZDDJotZKzAu-l5uq7_kyuaUD_XNpOp0cZHig0lOpigh3eOWqDbmmsm",
 //   stripeTestKey: "sk_test_Ma7vvXxqIKxpUoHmFNFv2oYt00u1nVjNkF"
 // };
+
+module.exports = {
+  HOST: "db-mysql-blr1-34758-do-user-14081312-0.b.db.ondigitalocean.com",
+  USER: "doadmin",
+  PASSWORD: "AVNS_mxiNlY-OmC3g7jdnbTR",
+  DB: "defaultdb",
+  PORT: 25060,
+  dialect: "mysql",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  stripeTestKey: "sk_test_Ma7vvXxqIKxpUoHmFNFv2oYt00u1nVjNkF"
+};
 
--- server.js
+++ server.js
@@ -36,7 +36,7 @@ require("./app/routes/payment.route")(app); // Payment route
 
 
 // Defining port
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 8045;
 // Listenning to port
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
